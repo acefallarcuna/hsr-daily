3a0d8d1333896ff7a70429ca1e6abd9a268da589
Ezekiel Fallarcuna <105767179+acefallarcuna@users.noreply.github.com>
2023-05-01 21:18:19 +0800

    Quick Fix
    Changed the time to 8:00 AM and 8:00 PM UTC+8

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const { Client, IntentsBitField } = require('discord.js');
 const cron = require('node-cron');
 const keepAlive = require("./server");
 const { BitlyClient } = require('bitly');
-require('dotenv').config();
 
 const client = new Client({
     intents: [
@@ -15,7 +14,7 @@ const client = new Client({
 const bitly = new BitlyClient(process.env.BITLY_ACCESS_TOKEN);
 
 client.once('ready', async () => {
-  console.log('Bot is ready!');
+  console.log('Pom-Pom is ready!');
 
   try {
     const longUrl = 'https://act.hoyolab.com/bbs/event/signin/hkrpg/index.html?act_id=e202303301540311&hyl_auth_required=true&hyl_presentation_style=fullscreen&utm_source=launcher&utm_medium=banner&utm_id=6';
@@ -23,8 +22,8 @@ client.once('ready', async () => {
 
     const embed = {
       color: 0x0099ff,
-      title: 'Attention everyone!',
-      description: 'Remember, consistent logins can lead to amazing rewards! So do not miss out on this opportunity and login now.',
+      title: 'Pom-Pom Here!',
+      description: 'Attention, consistent logins can lead to amazing rewards! So do not miss out on this opportunity and login now!',
       image: {
         url: 'https://upload-os-bbs.hoyolab.com/upload/2023/04/26/12c3c9e763465ea70330d6bc0891a300_829334094349192999.jpg?x-oss-process=image/resize,s_1000/quality,q_80/auto-orient,0/interlace,1/format,jpg',
       },
@@ -35,15 +34,27 @@ client.once('ready', async () => {
         },
       ],
     };
-
-    cron.schedule('0 0,12 * * *', () => {
+    
+    cron.schedule('0 8,20 * * *', () => {
       const channel = client.channels.cache.get(process.env.CHANNEL_ID);
-      channel.send(embed)
+        
+      // Pings a role
+      channel.send('<@&ROLE_ID>', {embeds: [embed]})
         .then(() => console.log('Message sent!'))
         .catch((error) =>
           console.error(`Error sending message: ${error.message}`)
         );
+      
+      // Sends the message
+      channel.send({embeds: [embed]})
+        .then(() => console.log('Message sent!'))
+        .catch((error) =>
+          console.error(`Error sending message: ${error.message}`)
+        );
+    }, {
+      timezone: 'Asia/Manila'
     });
+    
   } catch (error) {
     console.error(`Error setting up scheduled message: ${error.message}`);
   }
